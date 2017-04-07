ac4b6759dffd15aceac168fe6a6e3b4429992966
zhengmingjun <keshiim@163.com>
2017-04-07 14:22:19 +0800

    完成图形绘制

--- CoreGraphicsDemo/CGView3.swift
+++ CoreGraphicsDemo/CGView3.swift
@@ -16,9 +16,12 @@ class CGView3: UIView {
         // Drawing code
         super.draw(rect)
         
-        let context = UIGraphicsGetCurrentContext()
-        self.drawRectWithContext(context: context!)
-        self.drawRectByUIKitWithContext(context: context!)
+        let context: CGContext! = UIGraphicsGetCurrentContext()
+        self.drawRectWithContext(context: context)
+        self.drawRectByUIKitWithContext(context: context)
+        self.drawEllipse(context: context)
+        self.drawArc(context: context)
+        self.drawCure(context: context)
     }
 
     /// Context直接回执
@@ -44,6 +47,45 @@ class CGView3: UIView {
         
         UIColor.red.setFill()
         UIRectFill(rect2)
-        
     }
+    
+    
+    /// 绘制椭圆
+    ///
+    /// - Parameter context:
+    public func drawEllipse(context: CGContext) {
+        let rect = CGRect.init(x: 20, y: 180, width: 100, height: 120)
+        
+        context.addEllipse(in: rect)
+        UIColor.purple.set()
+        context.drawPath(using: CGPathDrawingMode.fill)
+    }
+    
+    /// 绘制弧形
+    ///
+    /// - Parameter context:
+    public func drawArc(context: CGContext) {
+        context.addArc(center: CGPoint.init(x: 200, y: 250), radius: 50, startAngle: 0, endAngle: .pi, clockwise: true)
+        UIColor.green.set()
+        context.drawPath(using: CGPathDrawingMode.fill)
+    }
+    
+    /// 绘制贝塞尔曲线
+    ///
+    /// - Parameter context: <#context description#>
+    func drawCure(context: CGContext) -> Void {
+       //绘制曲线
+        context.move(to: .init(x: 20, y: 310))
+        //绘制二次贝塞尔曲线
+        context.addQuadCurve(to: .init(x: 100, y: 400), control: .init(x: 220, y: 310))
+        
+        context.move(to: .init(x: 230, y: 310))
+        //绘制三次贝塞尔曲线
+        context.addCurve(to: .init(x: 300, y: 500), control1: .init(x: 100, y: 330), control2: .init(x: 400, y: 410))
+        //
+        UIColor.yellow.setFill()
+        UIColor.red.setStroke()
+        context.drawPath(using: CGPathDrawingMode.fillStroke)
+    }
+    
 }
---
+++ CoreGraphicsDemo/CGView4.swift
@@ -0,0 +1,26 @@
+//
+//  CGView4.swift
+//  CoreGraphicsDemo
+//
+//  Created by Jason on 2017/4/7.
+//  Copyright © 2017年 Jason. All rights reserved.
+//
+
+import UIKit
+
+class CGView4: UIView {
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+        
+       // let context: CGContext! = UIGraphicsGetCurrentContext()
+        
+        
+    }
+    
+    
+
+}
