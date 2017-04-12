31b4a1487be0bb2ef1426e22ac55788ef47ab75c
zhengmingjun <keshiim@163.com>
2017-04-12 19:16:00 +0800

    添加绘制图片
    添加绘制图片

--- CoreGraphicsDemo/CGView4.swift
+++ CoreGraphicsDemo/CGView4.swift
@@ -18,10 +18,13 @@ class CGView4: UIView {
         
         let context: CGContext! = UIGraphicsGetCurrentContext()
         self.drawText(context: context)
-        
+        self.drawImage(context: context)
     }
     
     
+    /// 绘制文字
+    ///
+    /// - Parameter context:
     public func drawText(context: CGContext) -> Void {
         let str = "使用CoreGraphics进行文字绘制使用CoreGraphics进行文字绘制使用CoreGraphics进行文字绘制使用CoreGraphics进行文字绘制使用CoreGraphics进行文字绘制使用CoreGraphics进行文字绘制使用CoreGraphics进行文字绘制使用CoreGraphics进行文字绘制使用CoreGraphics进行文字绘制"
         let rect = CGRect.init(x: 20, y: 20, width: 280, height: 200)
@@ -33,6 +36,13 @@ class CGView4: UIView {
         (str as NSString).draw(in: rect, withAttributes: [NSFontAttributeName: font,
                                                           NSForegroundColorAttributeName: color,
                                                           NSParagraphStyleAttributeName: style])
+    }
+    
+    public func drawImage(context: CGContext) {
+        let img = UIImage.init(named: "jieping")
+        //绘制到指定的矩形中，注意如果大小不合适会进行拉伸
+        img?.draw(in: .init(x: 0, y: 200, width: 100, height: 80))
+        img?.draw(at: .init(x: 0, y: 320))
     }
 
 }
